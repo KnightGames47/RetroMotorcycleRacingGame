f98e384a4aa39485a2d11711e1c6b91a7ab7c026
Jesse <jsengineer@yahoo.com>
2025-01-07 20:20:50 -0700

    Basic movement implementation, getting things working with input
    Working a lot on input systems and considering different approaches to input.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,98 @@
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour, MotorCycle_Input.IPlayerActions
+{
+    [Header("Movement Settings")]
+    [SerializeField] public float maxSpeed;
+    [SerializeField] public float acceleration;
+    [SerializeField] public float deceleration;
+
+    //movement variables
+    private bool isAccelerating = false;
+    private bool isDecelerating = false;
+    private float curSpeed = 0f;
+    private float turningDir = 0f;
+    private Vector3 playerMoveDirection;
+
+    //General references
+    private MotorCycle_Input motorCycleInput;
+    private Rigidbody rb;
+    private Collider collider;
+    private bool isGrounded;
+
+    #region Unity Callbacks-------------------------------------------------------------------------
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+    public void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        if (motorCycleInput == null)
+        {
+            motorCycleInput = new MotorCycle_Input();
+            motorCycleInput.Player.SetCallbacks(this);//We are hooking up the callbacks from the input to the ones here.
+        }
+
+        motorCycleInput.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        motorCycleInput.Player.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessMovement();
+    }
+
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Helper Functions------------------------------------------------------------------------
+    private void ProcessMovement()
+    {
+        //Need to figure out acceleration/decceleration later
+        Vector3 moveVector = transform.TransformDirection(playerMoveDirection) * maxSpeed;
+
+        rb.linearVelocity = new Vector3(moveVector.x, rb.linearVelocity.y, moveVector.z);
+    }
+
+    #endregion
+
+    #region Input Callbacks-------------------------------------------------------------------------
+    public void OnAccelerate(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Accelerating");
+    }
+
+    public void OnBrake(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnTurning(InputAction.CallbackContext context)
+    {
+        turningDir = context.ReadValue<float>();
+        //Debug.Log("Tuning dir: " + turningDir);
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        Vector3 moveDir = Vector3.zero;
+        moveDir.x = context.ReadValue<Vector2>().x;
+        moveDir.z = context.ReadValue<Vector2>().y;
+
+        playerMoveDirection = moveDir;
+    }
+    #endregion
+}
